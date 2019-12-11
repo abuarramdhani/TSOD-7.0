6c354d0d7a80508309b6dde41ec71acd8929fa0a
melanoke <melanoke@yahoo.com>
2019-12-11 11:15:19 +0700

    Update list_analisis.php
    tambah vitur cetak laporan

--- vpanel/mod/list_analisis.php
+++ vpanel/mod/list_analisis.php
@@ -1,20 +1,33 @@
 <?php
     error_reporting(1);
 	if (empty($_GET[modul])) $_GET[modul]='none';
-	else $_GET[modul]=$converter->decode($_GET[modul]);
+	else $_GET[modul]=$converter->decode($_GET[modul]);  
 ?>
+<style>
+    @media print {
+
+        .th2 {
+            display: table-cell !important;
+            border: 1px solid black !important;
+        }
+
+        .td1 {
+            border: 1px solid black !important;
+        }        
+    }   
+</style>
 <legend><img src="./assets/list.soal.png" width="48"> Analisis Soal <?php if (!empty($_GET[modul]) and $_GET[modul]!='none') {
   		$dt=mysql_fetch_row(mysql_query("SELECT nm_modul FROM modul WHERE kd_modul='$_GET[modul]'"));
 		echo "Modul $dt[0]";
   }
   ?></legend>
 
-<div class="box">            
-            <!-- /.box-header -->
-            <div class="box-body">
-							<div class="box-header"> Modul : 
-              <select name="menu1"  onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
-      <?php 			
+<div class="box">
+    <!-- /.box-header -->
+    <div class="box-body">
+        <div class="box-header no-print"> Modul :
+            <select name="menu1" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);" class="no-print">
+                <?php 			
 	  	$kode=$converter->encode('none');
 
 	    if ($_GET[modul]=="none") echo "<option value=\"to=$analisissoal&modul=$kode\" selected>pilih salah satu</option>";
@@ -31,21 +44,22 @@
 				echo "<option value=\"./?to=$analisissoal&modul=$kode\">$dt[1]</option>";
 		}
       ?>
-      </select>
-            </div>
-              <table class="table table-bordered table-striped">
-                <thead>
+            </select>
+            <input name="absen" type="button" class="no-print warning pull-right" onclick="window.print()" value="Cetak Analisis" />            
+        </div>
+        <table class="table table-bordered table-striped">
+            <thead>
                 <tr>
                     <th width="10">No.</th>
-	                <th>Soal</th>
-	                <th>Dikerjakan Sebanyak</th>
-	                <th>Siswa menjawab benar</th>
-	                <th>Siswa menjawab salah</th>
-	                <th>Siswa tidak menjawab</th>
+                    <th>Soal</th>
+                    <th width="50">Dikerjakan Sebanyak</th>
+                    <th width="50">Menjawab benar</th>
+                    <th width="50">Menjawab salah</th>
+                    <th width="50">Tidak menjawab</th>
                 </tr>
-                </thead>
-                <tbody>
- <?php              
+            </thead>
+            <tbody>
+                <?php              
     $hasil=mysql_query("SELECT kd_soal, q, a FROM soal WHERE  kd_modul='$_GET[modul]' ORDER BY kd_modul");   
 	while ($dt=mysql_fetch_array($hasil)) 
 	{
@@ -55,27 +69,27 @@
         $abstain=mysql_num_rows(mysql_query("SELECT kd_jawaban FROM jawaban WHERE`kd_soal`='$dt[kd_soal]' and `option`=''")); 
 ?>
                 <tr>
-                  <td><?= $i ?>. </td>
-                  <td><?= substr($dt[q],0,100); ?></td>
-                  <td><?= $jml ?></td>
-                  <td><?= $benar ?></td>
-                  <td><?= $salah ?></td>
-                  <td><?= $abstain ?></td>
-                </tr>     
-<?php } ?>									
-                </tbody>
-                <tfoot>
-<tr>
+                    <td><?= $i ?>. </td>
+                    <td><?= substr($dt[q],0,100); ?></td>
+                    <td><?= $jml ?></td>
+                    <td><?= $benar ?></td>
+                    <td><?= $salah ?></td>
+                    <td><?= $abstain ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+            <tfoot class="no-print">
+                <tr>
                     <th width="10">No.</th>
-	                <th>Soal</th>
-	                <th>Dikerjakan Sebanyak</th>
-	                <th>Siswa menjawab benar</th>
-	                <th>Siswa menjawab salah</th>
-	                <th>Siswa tidak menjawab</th>
-                </tr>                    
-                </tfoot>
-              </table>
-            </div>
+                    <th>Soal</th>
+                    <th>Dikerjakan Sebanyak</th>
+                    <th>Siswa menjawab benar</th>
+                    <th>Siswa menjawab salah</th>
+                    <th>Siswa tidak menjawab</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
     <script>
         $(document).ready(function() {
             $('#example1').dataTable({
@@ -88,5 +102,5 @@
         });
 
     </script>
-            <!-- /.box-body -->
-          </div>
+    <!-- /.box-body -->
+</div>
